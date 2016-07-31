b6c21a00c31b3ad23208393f8b5f55e53b657453
neelasantosh <neela.santosh2405@gmail.com>
2016-07-31 16:48:49 +0530

    PopUp

---
+++ FullNameScreen.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PopUpDemo
+{
+    public partial class FullNameScreen : UserControl
+    {
+        public FullNameScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FirstNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FirstNameTextBox.Text = "";
+        }
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Controls.Primitives;
+
+
+namespace PopUpDemo
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowPopUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Popup screen = new Popup();
+            //THis is out control
+            FullNameScreen control = new FullNameScreen();
+
+            //adding custom control to the popup
+            screen.Child = control;
+            screen.VerticalOffset = 100;
+            screen.HorizontalOffset = 100;
+
+            //adding event handler to the button on FullNameScreen Ok Button
+            //through anonymous method
+            control.OkButton.Click += delegate(object s, RoutedEventArgs rea)
+            {
+                if (control.FirstNameTextBox.Text != "" && control.LastNameTextBox.Text != "")
+                {
+                    FullNameTextBox.Text = control.FirstNameTextBox.Text + " " + control.LastNameTextBox.Text;
+                    screen.IsOpen = false;
+                }
+                else
+                {
+                    MessageBox.Show("ERROR: Enter values!");
+                }
+            };
+            control.CancelButton.Click += delegate(object s, RoutedEventArgs rea)
+            {
+                screen.IsOpen = false;
+            };
+            screen.IsOpen = true;
+        }
+    }
+}
